42fce791b06a22a6fae2d496af89d3012d66935d
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-12-18 22:46:39 -0400

    feat: Implement header's left section

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,42 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Header() {
   return (
     <div>
-      <h1>Header</h1>
 
-      {/* Left */}
+      <div
+        className='flex justify-between bg-white max-w-6xl'
+      >
+        {/* Left */}
+        <div
+          className='relative hidden lg:inline-grid w-24 cursor-pointer'
+        >
+          <Image 
+            src='https://links.papareact.com/ocw'
+            fill
+            style={{objectFit: 'contain'}}
+          />
+        </div>
 
-      {/* Middle */}
+        <div
+          className='relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer'
+        >
+          <Image 
+            src='https://links.papareact.com/jjm'
+            fill
+            style={{objectFit: 'contain'}}
+          />
+        </div>
 
-      {/* Right */}
+        <h1>login</h1>
+
+        {/* Middle */}
+
+        {/* Right */}
+
+
+      </div>
     </div>
   )
 }
