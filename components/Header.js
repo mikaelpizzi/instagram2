46a868e856ac3493e52a95f3dee57088a647470f
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-12-19 19:22:27 -0400

    feat: Implement sticky header

--- components/Header.js
+++ components/Header.js
@@ -14,8 +14,9 @@ import { HomeIcon } from '@heroicons/react/24/solid'
 
 function Header() {
   return (
-    <div>
-
+    <div
+      className='shadow-sm border-b bg-white sticky top-0 z-50'
+    >
       <div
         className='flex justify-between bg-white max-w-6xl mx-5 lg:mx-auto'
       >
