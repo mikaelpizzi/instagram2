825e702e0cbde0c45bb31a159354d6b683aa0022
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-12-23 17:27:07 -0400

    imp: Implement feed structure

--- components/Feed.js
+++ components/Feed.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import Stories from './Stories'
 
 function Feed() {
   return (
-    <div>
-      
-    </div>
+    <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
+      {/* Section */}
+      <section>
+        <Stories />
+        {/* Posts */}
+      </section>
+
+      {/* Section */}
+
+      <section>
+
+        {/* Mini profile */}
+        {/* Suggestions */}
+      </section>
+
+    </main>
   )
 }
 
