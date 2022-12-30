554d1e7ea123efa3d3124ca8b34b3c765a4da8e3
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-12-29 21:41:28 -0400

    feat: Implement suggestions

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { faker } from '@faker-js/faker';
 
 function Suggestions() {
+  const [ suggestions, setSuggestions ] = useState([]);
+
+  useEffect(() => {
+    const suggestions = [...Array(5)].map((_, i) => (
+      {
+        userId: faker.datatype.uuid(),
+        username: faker.internet.userName(),
+        email: faker.internet.email(),
+        avatar: faker.image.avatar(),
+        password: faker.internet.password(),
+        birthdate: faker.date.birthdate(),
+        registeredAt: faker.date.past(),
+        company: faker.company.name()
+      }
+    ))
+
+    setSuggestions(suggestions);
+  }, []);
+
   return (
-    <div>
-      
+    < div className='mt-4 ml-10'>
+      <div className='flex justify-between text-sm mb-5'>
+        <h3 className='text-sm font-bold text-gray-400'>Suggestions for you</h3>
+        <button className='text-gray-600 font-semibold'>See All</button>
+      </div>
+
+      { suggestions.map(profile => (
+        <div
+          key={profile.userId}
+          className='flex items-center justify-between mt-3'
+        >
+          <img
+            src={profile.avatar}
+            alt={`Avatar of ${profile.username}`}
+            className='w-10 h-10 rounded-full border border-gray-400 p-[2px]'
+          />
+
+          <div className='flex-1 ml-4'>
+            <h2 className='font-semibold text-sm'>{profile.username}</h2>
+            <h3 className='text-xs text-gray-400'>Works at {profile.company}</h3>
+          </div>
+
+          <button className='text-blue-400 text-xs font-bold ml-4'>Follow</button>
+        </div>
+
+
+
+      ))}
     </div>
   )
 }
