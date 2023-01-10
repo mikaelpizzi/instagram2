a6f7e36304d32c038f1c7037a7338b6af5e292a1
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-01-10 19:40:55 -0400

    feat: Implement modal content

--- components/Modal.js
+++ components/Modal.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import { useRecoilState } from 'recoil'
 import { modalState } from '../atoms/modalAtom';
 import { Dialog, Transition } from '@headlessui/react';
+import { CameraIcon } from '@heroicons/react/24/outline';
 
 function Modal() {
     const [ open, setOpen ] = useRecoilState(modalState);
@@ -50,7 +51,59 @@ function Modal() {
                     <div
                         className='inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6'
                     >
-                        <h1>Hello</h1>
+                        <div>
+                            <div
+                                // onClick={() => filePickerRef.current.click()}
+                                className='mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100 cursor-pointer'
+                            >
+                                <CameraIcon
+                                    className='w-6 h-6 text-red-600'
+                                    aria-hidden='true'
+                                />
+                            </div>
+
+                            <div>
+                                <div
+                                    className='mt-3 text-center sm:mt-5'
+                                >
+                                    <Dialog.Title
+                                        as='h3'
+                                        className='text-lg leading-6 font-medium text-gray-900'
+                                    >
+                                        Upload a photo
+                                    </Dialog.Title>
+
+                                    <div>
+                                        <input
+                                            // ref={filePickerRef}
+                                            type='file'
+                                            hidden
+                                            // onChange={addImageToPost}
+                                            />
+                                    </div>
+
+                                    <div className='mt-2'>
+                                        <input
+                                            className='border-none focus:ring-0 w-full text-center'
+                                            type='text'
+                                            // ref={captionRef}
+                                            placeholder='Please enter a caption'
+                                            />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div
+                                className='mt-5 sm:mt-6'
+                            >
+                                <button
+                                    type='button'
+                                    className='inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300'
+                                >
+                                    Upload Post
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </Transition.Child>
             </div>
