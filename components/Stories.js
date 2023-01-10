4e69776818a923c86b939fd1d9be2f816f4fd07c
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-01-09 20:22:02 -0400

    feat: Show logged user story icon

--- components/Stories.js
+++ components/Stories.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { faker } from '@faker-js/faker';
 import Story from './Story'
+import { useSession } from 'next-auth/react'
 
 
 function Stories() {
+    const { data: session } = useSession();
     
     const [suggestions, setSuggestions] = useState([]);
     useEffect(() => {
@@ -21,6 +23,12 @@ function Stories() {
 
     return (
         <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black'>
+            { session && (
+                <Story
+                    img={session.user.image}
+                    username={session.user.username}
+                />
+            )}
             { suggestions.map(profile => (
                 <Story
                     key={profile.userId}
