4b3fcbf9b30afde347116947f7646f96cbdf3dcb
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-01-10 20:02:47 -0400

    imp: Add necessary captionRef

--- components/Modal.js
+++ components/Modal.js
@@ -8,6 +8,7 @@ function Modal() {
     const [ open, setOpen ] = useRecoilState(modalState);
     const filePickerRef = useRef(null);
     const [ selectedFile, setSelectedFile ] = useState(null);
+    const captionRef = useRef(null);
 
     const addImageToPost = e => {
         const reader = new FileReader();
